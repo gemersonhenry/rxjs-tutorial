abe0d7851a8f85304ab1739b6c1928d50495b840
BZGemerson <henryingtel@gmail.com>
2019-09-08 11:41:09 -0500

    feat(config): closing configuration

--- src/app/subjects/subject/subject.component.ts
+++ src/app/subjects/subject/subject.component.ts
@@ -8,59 +8,13 @@ import { Subject, interval } from 'rxjs';
 })
 export class SubjectComponent implements OnInit {
 
-  public subject01 = new Subject<string>();
-  public subject02 = new Subject<string>();
+  public subject = new Subject<number>();
 
   public observerCounter01 = 0;
 
   constructor() {
-    this.customObserver01 = this.customObserver01.bind(this);
   }
 
   ngOnInit() {
-    this.runExample();
-  }
-
-  public get observable01$() {
-    return this.subject01.asObservable();
-  }
-
-  public get observable02$() {
-    return this.subject02.asObservable();
-  }
-
-  public customObserver01(value: string) {
-    this.observerCounter01++;
-    console.log('customObserver01 => value: ', value, this.observerCounter01);
-  }
-
-  public customObserver02(value: string) {
-    console.log('customObserver02 => value: ', value);
-  }
-
-  public runExample() {
-    const currentInterval = interval(5000);
-    currentInterval.subscribe((value) => {
-      console.log(value);
-      const text = `interval: ${value}`;
-      this.subject01.next(text);
-      this.subject02.next(text);
-    });
-  }
-
-  public subscribeObserver01ToSubject01() {
-    this.observable01$.subscribe(this.customObserver01);
-  }
-
-  public subscribeObserver02ToSubject01() {
-    this.observable01$.subscribe(this.customObserver02);
-  }
-
-  public subscribeObserver01ToSubject02() {
-    this.observable02$.subscribe(this.customObserver01);
-  }
-
-  public subscribeObserver02ToSubject02() {
-    this.observable02$.subscribe(this.customObserver01);
   }
 }
